6a6dbbb11389dfdf978a9d93f6dcff1b75cfaae2
Andrew Karpow <andy@mailbox.tu-berlin.de>
2013-06-05 23:49:15 +0200

    first commit

---
+++ extract.c
@@ -0,0 +1,130 @@
+/* gcc -o extract extract.c */
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <libgen.h>
+
+struct files {
+    char *filename;
+    uint32_t filename_len;
+    uint32_t file_len;
+    uint32_t offset;
+};
+
+static void _mkdir(const char *dir) {
+    char tmp[256];
+    char *p = NULL;
+    size_t len;
+ 
+    snprintf(tmp, sizeof(tmp),"%s",dir);
+    len = strlen(tmp);
+    if(tmp[len - 1] == '/')
+        tmp[len - 1] = 0;
+    for(p = tmp + 1; *p; p++)
+        if(*p == '/') {
+            *p = 0;
+            mkdir(tmp, S_IRWXU);
+            *p = '/';
+        }
+    mkdir(tmp, S_IRWXU);
+}
+
+int main(int argc, char *argv[]) {
+    struct files *files_array;
+    struct stat s;
+    void *file_ptr;
+    uint32_t num_files, i; 
+    int fd;
+    char *cur_header_offset;
+    
+
+    if(argc != 2)
+        goto usage;
+
+    fd = open(argv[1], O_RDONLY);
+    if(fd < 0)
+        goto usage;
+
+    file_ptr = mmap(NULL, s.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    close(fd);
+
+    if(file_ptr == MAP_FAILED)
+    {
+        fprintf(stderr, "Allocation failed\n");
+        return -1;
+    }
+
+    if(((uint32_t *)file_ptr)[0] != 0x7846)
+    {
+        fprintf(stderr, "Magic not found, wrong file?\n");
+        return -1;
+    }
+
+    num_files = ((uint32_t *)file_ptr)[1];
+    printf("Found %u files\n", num_files);
+    cur_header_offset = 8 + (char *)file_ptr;
+
+    files_array = malloc(sizeof(struct files) * num_files);
+
+    for(i = 0; i < num_files; i++)
+    {
+        files_array[i].filename_len = *(uint32_t *)cur_header_offset;
+        cur_header_offset += 4;
+        
+        files_array[i].filename = cur_header_offset;
+        cur_header_offset += files_array[i].filename_len;
+        
+        files_array[i].file_len = *(uint32_t *)cur_header_offset;
+        cur_header_offset += 4; 
+        
+        files_array[i].offset = *(uint32_t *)cur_header_offset;
+        cur_header_offset += 4; 
+    }
+
+    for(i = 0; i < num_files; i++)
+    {
+        char *filepath = strndup(files_array[i].filename,
+                files_array[i].filename_len);
+        char *filedir = dirname(strdup(filepath));
+
+        if(stat(filedir, &s))
+        {
+            _mkdir(filedir);
+        }
+
+        if((fd = open(filepath, O_WRONLY|O_CREAT|O_EXCL, 
+            S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP)) == -1)
+        {
+            fprintf(stderr, "Error creating file %s\n", filepath);
+            return -1;
+        }
+
+        if(write(fd, cur_header_offset + files_array[i].offset, 
+                    files_array[i].file_len) == files_array[i].file_len)
+        {
+            printf("File \"%s\" (size %u bytes) extracted.\n", 
+                    filepath, files_array[i].file_len);
+        }
+        else
+        {
+            fprintf(stderr, "Error writing file %s\n", filepath);
+            return -1;
+        }
+        free(filedir);
+        free(filepath);
+        close(fd);
+    }
+    return 0;
+
+usage:
+    printf("Usage: %s [FILE]\n", argv[0]);
+    return -1;    
+}
+
